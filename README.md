1e2c2e6fcc68fcacd42f18e949402ac4d885a7f9
Didier van Os <dvos@sogyo.nl>
2023-10-13 16:21:44 +0200

    Make child

--- README.md
+++ README.md
@@ -7,3 +7,7 @@ This library provides routing for react.
 ## Route Errors
 
 Routes can throw errors, e.g. 404 errors. The `useRouteError` returns the error. You can create an ErrorPage component and configure it as the `errorElement`
+
+## Layouts and children
+
+You can configure children with their own `path`. These will then be rendered in the `<Outlet />` of the parent component.
--- reactrouter-6.16.0/src/main.jsx
+++ reactrouter-6.16.0/src/main.jsx
@@ -14,11 +14,13 @@ const router = createBrowserRouter([
     path: "/",
     element: <Root />,
     errorElement: <ErrorPage />,
+    children: [
+      {
+        path: "contacts/:contactId",
+        element: <Contact />,
+      },
+    ],
   },
-  {
-    path: "contacts/:contactId",
-    element: <Contact />,
-  }
 ]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
